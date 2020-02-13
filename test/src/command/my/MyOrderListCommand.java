582b38d7ba4a4d561506dac303e8ff12f22f072e
User <User@YD03-01>
2020-02-13 16:54:59 +0900

    t

--- test/src/command/my/MyOrderListCommand.java
+++ test/src/command/my/MyOrderListCommand.java
@@ -8,13 +8,13 @@ import javax.servlet.http.HttpServletResponse;
 
 import command.Command;
 
-public class MyCouponCommand implements Command {
+public class MyOrderListCommand implements Command {
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		return "HTML/lsj/myCoupon.jsp";
+		return "HTML/lsj/myOrderList.jsp";
 	}
 
 }
